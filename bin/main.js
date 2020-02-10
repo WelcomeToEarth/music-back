e4e858693c0d266675ce3ce02712e5902a4f257b
张飞飞 <zhangfei@xinhua.org>
2020-02-10 11:45:16 +0800

    mongo 只允许本地访问

--- bin/main.js
+++ bin/main.js
@@ -1,22 +1,20 @@
-const mongoose=require('mongoose')
-const http=require('http')
+const mongoose = require('mongoose')
+const http = require('http')
 require('../model')
-let app=require('../app')
-let server=http.createServer(app)
+let app = require('../app')
+let server = http.createServer(app)
 
-mongoose.connect("mongodb://39.100.123.183/music",function (err) {
+mongoose.connect("mongodb://127.0.0.1/music", function(err) {
     console.log("mongodb 已连接!")
-    if(!err){
-        server.listen(3000,function (err) {
-            if(!err){
+    if (!err) {
+        server.listen(3000, function(err) {
+            if (!err) {
                 console.log("express 服务器已打开 ")
             }
         })
     }
 })
 
-server.on('close',function () {
+server.on('close', function() {
     mongoose.disconnect()
 })
-
-
